f7b174ccce8be22059156e5bb8a41b102a3f48d2
lowki <budain.kevin@yahoo.fr>
2014-02-25 14:58:12 +0100

    add fos rest bundle and update composer.lock

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new Terra\NovaBundle\TerraNovaBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {

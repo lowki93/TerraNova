598f16b24a5c5e352c43524741cd2e42d1342d9a
lowki <budain.kevin@yahoo.fr>
2014-02-27 11:26:06 +0100

    add profil edit

---
+++ src/Terra/NovaBundle/Form/Type/ProfileFormType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Terra\NovaBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Security\Core\Validator\Constraint\UserPassword as OldUserPassword;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
+ 
+class ProfileFormType extends BaseType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        if (class_exists('Symfony\Component\Security\Core\Validator\Constraints\UserPassword')) {
+            $constraint = new UserPassword();
+        } else {
+            // Symfony 2.1 support with the old constraint class
+            $constraint = new OldUserPassword();
+        }
+
+        $this->buildUserForm($builder, $options);
+
+        $builder->add('current_password', 'password', array(
+            'label' => 'Password',
+            'translation_domain' => 'FOSUserBundle',
+            'mapped' => false,
+            'constraints' => $constraint,
+        ));
+    }
+
+    public function getName()
+    {
+        return 'terra_nova_profile_edit';
+    }
+
+    /**
+     * Builds the embedded form representing the user.
+     *
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    protected function buildUserForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', null, array('label' => 'Username', 'translation_domain' => 'FOSUserBundle'))
+            ->add('email', 'email', array('label' => 'Email', 'translation_domain' => 'FOSUserBundle'))
+        ;
+    }
+}

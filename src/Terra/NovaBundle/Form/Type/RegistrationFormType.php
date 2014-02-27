677e474f39059ba279dca10b10f67930dc14538d
lowki <budain.kevin@yahoo.fr>
2014-02-27 10:46:12 +0100

    add register

---
+++ src/Terra/NovaBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Terra\NovaBundle\Form\Type;
+ 
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+ 
+class RegistrationFormType extends BaseType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', 'email', array('label' => 'Email', 'translation_domain' => 'FOSUserBundle'))
+            ->add('username', null, array('label' => 'Username', 'translation_domain' => 'FOSUserBundle'))
+            ->add('plainPassword', 'repeated', array(
+                'type' => 'password',
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => 'Password'),
+                'second_options' => array('label' => 'Password Confirmation'),
+                'invalid_message' => 'fos_user.password.mismatch',
+            ))
+        ;
+    }
+ 
+    public function getName()
+    {
+        return 'terra_nova_registration';
+    }
+}

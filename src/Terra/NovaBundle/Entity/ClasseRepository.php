e0c1fc1e6534f85308499efb193d218fce67a78b
lowki93 <budain.kevin@yahoo.Fr>
2014-03-19 23:15:48 +0100

    change namme of class repository

--- src/Terra/NovaBundle/Entity/ClasseRepository.php
+++ src/Terra/NovaBundle/Entity/ClasseRepository.php
@@ -1,13 +1,15 @@
 <?php
 
+namespace Terra\NovaBundle\Entity;
+
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\QueryBuilder;
 
-class ClassRepository extends EntityRepository {
+class ClasseRepository extends EntityRepository {
 
     public function findByEnseignant($idEnseignant) {
         $qb = $this->createQueryBuilder('c');
-        $qb->where('c.enseignant', ':id'))
+        $qb->where('c.enseignant', ':id')
             ->setParameter('id' , $idEnseignant);
 
         return $classe = $qb->getQuery()->getResult();

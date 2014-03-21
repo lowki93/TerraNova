b3f665eae0a2bf2ddd5a819424c1c7a45918405d
lowki93 <budain.kevin@yahoo.Fr>
2014-03-21 17:35:19 +0100

    use security for authentification

--- src/Terra/NovaBundle/Controller/EnseignantController.php
+++ src/Terra/NovaBundle/Controller/EnseignantController.php
@@ -12,15 +12,10 @@ class EnseignantController extends Controller
 	{
 		// test for user is conected
 		$user = $this->container->get('security.context')->getToken()->getUser();
-		if (!is_object($user) ) {
-			$url = $this->container->get('router')->generate('terra_nova_enseigant_connection');
-			return $response = new RedirectResponse($url);
-		} else {
-			$idEnseignant = $user->getId();
-			$classe = $this->getDoctrine()->getRepository('TerraNovaBundle:Classe')->findByEnseignant($idEnseignant);
-			return $this->render('TerraNovaBundle:Enseignant:index.html.twig', array( 'user' => $user,
+		$idEnseignant = $user->getId();
+		$classe = $this->getDoctrine()->getRepository('TerraNovaBundle:Classe')->findByEnseignant($idEnseignant);
+		return $this->render('TerraNovaBundle:Enseignant:index.html.twig', array( 'user' => $user,
 																					   'classe' => $classe));
-		}
 	}
 
 }

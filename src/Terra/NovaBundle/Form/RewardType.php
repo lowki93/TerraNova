9bc47be921b27a70bb950863f76cf9e7f006da28
lowki93 <budain.kevin@yahoo.Fr>
2014-04-10 14:31:34 +0200

    add badge entity

---
+++ src/Terra/NovaBundle/Form/RewardType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Terra\NovaBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class RewardType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Terra\NovaBundle\Entity\Reward'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'terra_novabundle_reward';
+    }
+}

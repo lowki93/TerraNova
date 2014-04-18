437a0d7b64d6017be0dc50e1e95b9ea4e1de3b9c
lowki93 <budain.kevin@yahoo.Fr>
2014-04-19 00:36:09 +0200

    add form contact

--- src/Terra/NovaBundle/Controller/DefaultController.php
+++ src/Terra/NovaBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace Terra\NovaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Terra\NovaBundle\Form\ContactType;
 
 class DefaultController extends Controller
 {
@@ -10,4 +12,41 @@ class DefaultController extends Controller
     {
         return $this->render('TerraNovaBundle:Homepage:index.html.twig');
     }
+
+	public function contactAction(Request $request)
+	{
+	    $form = $this->createForm(new ContactType());
+
+	    if ($request->isMethod('POST')) {
+	        $form->bind($request);
+
+	        if ($form->isValid()) {
+	            $message = \Swift_Message::newInstance()
+	                ->setSubject($form->get('subject')->getData())
+	                ->setFrom($form->get('email')->getData())
+	                ->setTo('contact@example.com')
+	                ->setBody(
+	                    $this->renderView(
+	                        'LCWebsiteBundle:Mail:contact.html.twig',
+	                        array(
+	                            'ip' => $request->getClientIp(),
+	                            'name' => $form->get('name')->getData(),
+	                            'message' => $form->get('message')->getData()
+	                        )
+	                    )
+	                );
+
+	            $this->get('mailer')->send($message);
+
+	            $request->getSession()->getFlashBag()->add('success', 'Your email has been sent! Thanks!');
+
+	            return $this->redirect($this->generateUrl('terra_nova_homepage'));
+	        }
+	    }
+
+	    return $this->render('TerraNovaBundle:Homepage:contact.html.twig', array(
+            'form'   => $form->createView(),
+        ));
+	}
+
 }
---
+++ src/Terra/NovaBundle/Form/ContactType.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Terra\NovaBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Collection;
+
+class ContactType extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', 'text', array(
+                'attr' => array(
+                    'placeholder' => 'What\'s your name?',
+                    'pattern'     => '.{2,}' //minlength
+                )
+            ))
+            ->add('firstName', 'text', array(
+                'attr' => array(
+                    'placeholder' => 'What\'s your firstName?',
+                    'pattern'     => '.{2,}' //minlength
+                )
+            ))
+            ->add('email', 'email', array(
+                'attr' => array(
+                    'placeholder' => 'So I can get back to you.'
+                )
+            ))
+			->add('youAre', 'choice', array(
+					'choices' => array(
+						'Enseignant' => 'Enseignant', 
+						'Parent' => 'Parent'
+					)
+			))
+            ->add('message', 'textarea', array(
+                'attr' => array(
+                    'cols' => 90,
+                    'rows' => 10,
+                    'placeholder' => 'And your message to me...'
+                )
+            ));
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $collectionConstraint = new Collection(array(
+            'name' => array(
+                new NotBlank(array('message' => 'Name should not be blank.')),
+                new Length(array('min' => 2))
+            ),
+            'firstName' => array(
+                new NotBlank(array('message' => 'firstName should not be blank.')),
+                new Length(array('min' => 2))
+            ),
+            'email' => array(
+                new NotBlank(array('message' => 'Email should not be blank.')),
+                new Email(array('message' => 'Invalid email address.'))
+            ),
+            'message' => array(
+                new NotBlank(array('message' => 'Message should not be blank.')),
+                new Length(array('min' => 5))
+            )
+        ));
+
+        $resolver->setDefaults(array(
+            'constraints' => $collectionConstraint
+        ));
+    }
+
+    public function getName()
+    {
+        return 'contact';
+    }
+}
